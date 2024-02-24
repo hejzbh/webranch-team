612fab0d86f98510855d5466b433613090adbe0a
hejzbh <hazimba123@gmail.com>
2024-02-24 17:13:14 +0100

    best practices

--- app/components/ui/PreTitle/PreTitle.tsx
+++ app/components/ui/PreTitle/PreTitle.tsx
@@ -12,14 +12,14 @@ const PreTitle = ({ className = "", text, upperCase }: PreTitleProps) => {
   text = upperCase ? text.toUpperCase() : text;
 
   return (
-    <h3
+    <h4
       style={{ lineHeight: "18px" }}
       className={` text-common-black-2 text-[14px] xxs:text-[15px] text-center md:text-[16px] rounded-3xl shadow-md bg-white max-w-fit py-3 px-5 font-semibold flex items-center ${className}`}
     >
       <span className={`mr-[8px] p-[6px] rounded-full bg-[#6864ED]`}></span>
       {text}
       <span className={`ml-[8px] p-[6px] rounded-full bg-[#6864ED]`}></span>
-    </h3>
+    </h4>
   );
 };
 
