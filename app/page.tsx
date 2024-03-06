5389eea7e0c95f264a11c8e5340e9f634afbc11f
hejzbh <hazimba123@gmail.com>
2024-03-06 18:35:09 +0100

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 // Next
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
 // Components
 const Hero = dynamic(() => import("@/app/components/Hero"));
 const CreativeProcess = dynamic(
@@ -11,6 +12,26 @@ const OurWork = dynamic(() => import("@/app/components/OurWork"));
 const Lighthouse = dynamic(() => import("@/app/components/Lighthouse"));
 const Testimonials = dynamic(() => import("@/app/components/Testimonials"));
 const Blog = dynamic(() => import("@/app/components/Blog"));
+
+export const metadata: Metadata = {
+  title: "Webranch - Your Digital Solutions Partner",
+  description:
+    "Discover top-quality digital solutions at Webranch. From web development to app development, e-commerce solutions, and custom coding, trust our dynamic team led by Amel and Hazim to guide you towards success. Explore our portfolio now.",
+  keywords: [
+    "Digital solutions",
+    "Web development",
+    "App development",
+    "E-commerce solutions",
+    "Custom coding",
+    "Webflow integration",
+    "Game development",
+    "Amel and Hazim",
+    "Dynamic team",
+    "Top-quality services",
+    "Upwork",
+  ],
+  applicationName: "Webranch",
+};
 
 export default function Home() {
   return (
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -9,6 +9,27 @@ const OurServices = dynamic(() => import("@/app/components/OurServices"));
 const WhyWorkWithUs = dynamic(() => import("@/app/components/WhyWorkWithUs"));
 const ContactUs = dynamic(() => import("@/app/components/ContactUs"));
 
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Our Services - Webranch",
+  description:
+    "Explore our comprehensive range of digital services at Webranch. From web development to app development, e-commerce solutions, and custom coding, trust our dynamic team led by Amel and Hazim to guide you towards success.",
+  keywords: [
+    "Digital services",
+    "Web development",
+    "App development",
+    "E-commerce solutions",
+    "Custom coding",
+    "Webflow integration",
+    "Game development",
+    "Amel and Hazim",
+    "Dynamic team",
+    "Top-quality services",
+    "Upwork",
+  ],
+  applicationName: "Webranch",
+};
+
 const ServicesPage = () => {
   return (
     <main className="min-h-screen">
