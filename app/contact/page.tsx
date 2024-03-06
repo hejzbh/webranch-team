d48fe3a8b3f2d22cb1e4b2b1efd771682ddcc3df
hejzbh <hazimba123@gmail.com>
2024-03-06 20:37:43 +0100

    Webranch contact us seo

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 // Next
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
 // Constants
 import { getContactBreadcrumbs } from "../constants/breadcrumbs";
 // Components
 const PageHeading = dynamic(() => import("@/app/components/PageHeading"));
 const ContactUs = dynamic(() => import("@/app/components/ContactUs"));
+
+export const metadata: Metadata = {
+  title: "Contact Us - Get in Touch with Webranch",
+  description:
+    "Connect with Webranch and explore how we can assist you with your digital needs. Whether you have questions, inquiries, or project ideas, we're here to help. Reach out to the dynamic duo of Amel and Hazim today.",
+  keywords: [
+    "contact us",
+    "Webranch contact",
+    "digital solutions",
+    "web development",
+    "app development",
+    "Webflow",
+    "freelance",
+    "UpWork",
+    "Amel",
+    "Hazim",
+  ],
+  openGraph: {
+    images: [
+      "https://i.ibb.co/pbbvCYR/White-Dusk-Blue-And-Nile-Blue-Geometric-Web-Design-And-Development-Presentation-1.jpg",
+    ],
+  },
+};
 
 const ContactPage = () => {
   return (
