22deabfe6a63f3e7c65327d85785743ce17b96d6
hejzbh <hazimba123@gmail.com>
2024-03-06 18:21:25 +0100

    edit

--- app/components/WebonifyDetails/WebonifyBenefits/WebonifyBenefits.tsx
+++ app/components/WebonifyDetails/WebonifyBenefits/WebonifyBenefits.tsx
@@ -20,7 +20,7 @@ const WebonifyBenefits = ({
     <section className={`${sectionSpacingClass} ${className}`}>
       <ul className="space-y-20">
         {benefits?.map((benefit, idx) => (
-          <li data-aos="fade-in" key={idx}>
+          <li key={idx}>
             <WebonifyBenefitCard benefit={benefit} reversed={idx % 2 === 0} />
           </li>
         ))}
