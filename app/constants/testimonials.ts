ab8611f57342e74deff79ab9ddb9764e7d0ecfb1
hejzbh <hazimba123@gmail.com>
2024-03-07 19:26:30 +0100

    new testimonial

--- app/constants/testimonials.ts
+++ app/constants/testimonials.ts
@@ -2,6 +2,14 @@ import { Testimonial } from "../types/testimonials";
 
 export const testimonialList: Testimonial[] = [
   {
+    author: "Phobos",
+    message:
+      "Webranch was an amazing seller to work with. I am extremely pleased with their excellent communication, high level of commitment, and eagerness to ensure that their buyers are always satisfied. They delivered work of top-notch quality and followed my instructions to the T. The website they created for me is truly unique, and I will definitely be a returning customer in the future.",
+    isUpworkClient: true,
+    authorProfileSRC: "",
+    authorRole: "/images/clients/universal.png",
+  },
+  {
     author: "Bojan Lovsin",
     message:
       "Amel and Hazim, long-time programmers at my company, were a joy to work with. Their expertise, positivity, and willingness to help made them my go-to for all digital needs. Wishing them luck as they team up on UpWork.",
