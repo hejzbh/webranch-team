e478e42ea3f6a43a8a15eb325379bb81ce451232
hejzbh <hazimba123@gmail.com>
2024-04-03 15:18:40 +0200

    new email

--- app/constants/contact.ts
+++ app/constants/contact.ts
@@ -1,3 +1,3 @@
-export const EMAIL = "webranch@team.com";
+export const EMAIL = "contact@webranch.team";
 export const LOCATION = "Remote";
 export const PHONE_NUMBERS: string[] = ["+387 62 657 438", "+387 60 312 1270"];
